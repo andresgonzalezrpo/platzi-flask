fd1dcc5f257bb26a7556b6a5488111fdf23d2ac1
Andrés González <afgonzalezr9003@gmail.com>
2024-07-24 21:00:59 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY']= 'SUPER SECRETO'
+
+todos = ['buy coffe','send request','deliver video to productor']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario',validators=[DataRequired()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
+
+@app.errorhandler(500)
+def server_internal_error(error):
+    return render_template('500.html',error=error)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html',error=error)
+
+@app.route("/")
+def index():
+    user_ip= request.remote_addr
+    
+    response = make_response(redirect('/hello'))
+    session['user_ip']= user_ip    
+    #response.set_cookie('user_ip',user_ip)
+    return response
+
+@app.route('/hello', methods = ['GET','POST'])
+def hello():
+    #user_ip = request.cookies.get('user_ip')
+    login_form = LoginForm()
+    user_ip =  session.get('user_ip')
+    username = session.get('username')
+    context = {'user_ip':user_ip,
+               'todos':todos,
+               'login_form':login_form,
+               'username': username
+    }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+        flash('Username successfully')
+        return redirect(url_for('index'))
+
+    return render_template('hello.html',**context) # ** expand the dictionary
+
+    #return "Hello world Flask, your IP is {}".format(user_ip)
